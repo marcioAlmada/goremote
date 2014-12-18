21459eb57afb27de38a408fe381c07f5f28a3c71
Márcio Almada <marcio3w@gmail.com>
2014-12-18 09:45:21 -0300

    refactory on main.go so multiple user interfaces can be plugged in

--- main.go
+++ main.go
@@ -3,92 +3,93 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"os/signal"
 	"strings"
 
-	"marcioAlmada/tv/upnp"
 	"github.com/jessevdk/go-flags"
-	"github.com/errnoh/gocurse/curses"
+	"github.com/marcioAlmada/goremote/upnp"
 )
 
+type application interface {
+	PromptPinCode() string
+	Run(upnp.Client, keyMap, keyMap) error
 }
 
 type options struct {
-	Args struct { IP string } `positional-args:"yes" required:"yes"`
-	REPL bool `short:"r" long:"repl" description:"Use a command line REPL session instead of gui"`
+	Args struct{ IP string } `positional-args:"yes" required:"yes"`
+	REPL bool                `short:"r" long:"repl" description:"Use a command line REPL session instead of gui"`
 }
 
 func main() {
-	var options options;
+	storagePath := os.Getenv("HOME") + "/.gocontroller/"
+
+	os.MkdirAll(storagePath, 0777) // setup
+
+	var options options
 	_, e := flags.Parse(&options)
 	nuke(e)
 
-	client := upnp.NewClient(options.Args.IP)
+	var app application
+	if options.REPL {
+		app = newCursesApplication()
+	} else {
+		app = newGtkApplication()
+	}
+
+	client := upnp.NewClient(options.Args.IP, fileGetContents(storagePath+options.Args.IP))
+
 	response, e := client.Handshake()
 	nuke(e, "Could not find device.")
 
 	if 401 == response.StatusCode { // authorize in case client is not authorized yet
-		pin := prompt("Enter provided pin code: ") // get pin code from devie
+		pin := app.PromptPinCode() // get pin code from device
 		response, e = client.Authorize(pin)
 		nuke(e, "Authentication failed. Is the pin code right?")
 	}
 
-	if 200 == response.StatusCode { // let's get UPnP control list from device
-		_, e := client.RequestControlsList()
-		runREPL(client)
-		nuke(e)
+	if 200 == response.StatusCode { // authenticated
+		_, e := client.RequestControlsList() // let's get UPnP control list from device
+		nuke(e, "Maybe device is off?")
+		client.RequestSystemInformation()
+		filePutContents(storagePath+client.IP, client.Mac)    // cache device info
+		e = app.Run(client, defaultKeyMap, alternativeKeyMap) // run!
+		nuke(e, "Failed to launch application")
 	}
 }
 
-func runREPL(client upnp.Client) {
-	// handle process termination
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt)
-	go func() {
-		<-c
-		curses.Endwin()
-		os.Exit(0)
-	}()
-	defer curses.Endwin()
-	// curses gui
-	curses.Noecho()
-	screen, _ := curses.Initscr()
-	screen.Keypad(true) // interpret escape sequences
-	screen.Addstr(0, 0, fmt.Sprintf("Use the keyboard:"), 0)
-	screen.Move(0, 1)
-	// add alternative key bidings to actionsMap
-	actionsMap.Merge(alternativeMap)
-	// run the REPL!
-	for {
-		keyCode := screen.Getch()
-		key, ok := actionsMap[keyCode]
-		if ok { // is the key mapped? Otherwise ignore it
-			signal, ok, _, _ := client.SendCommand(key.Command)
-			if ok { // show status on screen when request is made
-				screen.Move(0, 1)
-				screen.Clrtoeol()
-				screen.Addstr(0, 1, fmt.Sprintf("%s (%s) >> %s", key.Command, signal, client.IP), 0)
-			}
-		}
-	}
-}
-
-func prompt(message string) (str string) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print(message)
-	str, err := reader.ReadString('\n')
-	nuke(err, "Could not read input.")
-	str = strings.TrimRight(str, "\n\r")
-	return
-}
-
-func nuke(err error, msg ...string) {
+func nuke(err error, msgs ...interface{}) {
 	if err != nil {
-		if(len(msg) > 1){
-			fmt.Fprintln(os.Stderr, msg[1])
+		if len(msgs) > 0 {
+			fmt.Fprintln(os.Stderr, msgs...)
 		}
 		fmt.Fprintln(os.Stderr, "error:", err)
 		os.Exit(1)
 	}
+}
+
+func getStoragePath() string {
+	return os.Getenv("HOME") + "/.gocontroller/"
+}
+
+func fileGetContents(filename string) string {
+	fp, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return ""
+	}
+	defer fp.Close()
+	reader := bufio.NewReader(fp)
+	bytes, _ := ioutil.ReadAll(reader)
+	contents := strings.TrimRight(string(bytes), "\n\r")
+	return contents
+}
+
+func filePutContents(filename string, content string) error {
+	fp, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+	_, err = fp.Write([]byte(content))
+	return err
 }
